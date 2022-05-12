49baf75229f77a6fffe0a550be6fda9d04ea4bc2
LennardJones <l.asanin@mail.ru>
2022-05-12 18:02:06 +0400

    Initial commit

---
+++ src/main/java/org/leonidasanin/algorithmbasedsolvedtasks/AlgorithmBasedSolvedTasksApplication.java
@@ -0,0 +1,13 @@
+package org.leonidasanin.algorithmbasedsolvedtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmBasedSolvedTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmBasedSolvedTasksApplication.class, args);
+	}
+
+}
