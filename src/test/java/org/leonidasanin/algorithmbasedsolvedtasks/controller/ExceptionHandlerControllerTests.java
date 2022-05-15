d326d1220aeb540930c1b779bbfcdb425633e84a
LennardJones <l.asanin@mail.ru>
2022-05-15 16:14:42 +0400

    Test method of ExceptionHandlerControllerTests class was implemented

--- src/test/java/org/leonidasanin/algorithmbasedsolvedtasks/controller/ExceptionHandlerControllerTests.java
+++ src/test/java/org/leonidasanin/algorithmbasedsolvedtasks/controller/ExceptionHandlerControllerTests.java
@@ -1,12 +1,37 @@
 package org.leonidasanin.algorithmbasedsolvedtasks.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.leonidasanin.algorithmbasedsolvedtasks.exception.TaskException;
+import org.leonidasanin.algorithmbasedsolvedtasks.model.SortedStringArrayIntersectionTask;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class ExceptionHandlerControllerTests {
+
+    @Mock
+    RedirectAttributes redirectAttributes;
 
     @Test
     void handleIOException() {
+        //given
+        var previousTask = new SortedStringArrayIntersectionTask();
+        var message = "message";
+        var exception = new TaskException(message, previousTask);
+        var exceptionHandler = new ExceptionHandlerController();
+
+        //when
+        var result = exceptionHandler.handleIOException(exception, redirectAttributes);
+
+        //then
+        Assertions.assertEquals("redirect:/", result);
+        Mockito.verify(redirectAttributes).addFlashAttribute("task", previousTask);
+        Mockito.verify(redirectAttributes).addFlashAttribute("error", message);
     }
 }
